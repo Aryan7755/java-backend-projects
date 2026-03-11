2a06c0ed4c8f71be3e445339b79df7735c602d56
ARYAN RAJ <aryanbanthu@gmail.com>
2026-03-11 23:43:09 +0530

    Add Book class with properties and display method

--- library-management-system/Book.java
+++ library-management-system/Book.java
@@ -1,1 +1,17 @@
+class Book {
+    int id;
+    String title;
+    String author;
+    boolean isIssued;
 
+    public Book(int id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isIssued = false;
+    }
+
+    void display() {
+        System.out.println(id + " " + title + " " + author + " Issued: " + isIssued);
+    }
+}
