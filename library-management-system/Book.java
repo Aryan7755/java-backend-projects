e42735ffda82a4e3b8cf424eeebe976afa376353
Aryan raj <aryanbanthu@gmail.com>
2026-03-13 12:02:34 +0530

    Implemented encapsulation and added setters.

--- library-management-system/Book.java
+++ library-management-system/Book.java
@@ -23,6 +23,23 @@ class Book {
         return author;
     }
 
+    public String getGenre() {
+        return genre;
+    }
+
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
     public boolean isIssued() {
         return isIssued;
     }
