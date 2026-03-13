69849fdcb62d06568d94116e9dc34e4d5d5d2b1b
Aryan raj <aryanbanthu@gmail.com>
2026-03-13 11:35:02 +0530

    Modified Book class and created Library class with 2 features - addBook and removeBook.

--- library-management-system/Book.java
+++ library-management-system/Book.java
@@ -1,18 +1,18 @@
 class Book {
-    int id;
+    int bookId;
     String title;
     String author;
     boolean isIssued;
 
     public Book(int id, String title, String author) {
-        this.id = id;
+        this.bookId = id;
         this.title = title;
         this.author = author;
         this.isIssued = false;
     }
 
     public int getId() {
-        return id;
+        return bookId;
     }
 
     public String getTitle() {
@@ -28,6 +28,6 @@ class Book {
     }
 
     void display() {
-        System.out.println(id + " " + title + " " + author + " Issued: " + isIssued);
+        System.out.println(bookId + " " + title + " " + author + " Issued: " + isIssued);
     }
 }
---
+++ library-management-system/Library.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+
+public class Library {
+    HashMap<Integer,Book> bookMap;
+    HashMap<Integer,Member> membersMap;
+    HashMap<Integer,Book> issuedBookMap;
+
+    public Library(HashMap<Integer, Book> bookMap, HashMap<Integer, Member> membersMap, HashMap<Integer, Book> issuedBookMap) {
+        this.bookMap = bookMap;
+        this.membersMap = membersMap;
+        this.issuedBookMap = issuedBookMap;
+    }
+
+    public void addBook(Book book){
+        bookMap.put(book.getId(), book);
+        System.out.println("Book added successfully.");
+    }
+    public void removeBook(int bookId) {
+        if (bookMap.containsKey(bookId)) {
+            bookMap.remove(bookId);
+            System.out.println("Book removed.");
+        } else {
+            System.out.println("Book not found.");
+        }
+    }
+
+    public void updateBook(int bookId, String title, String author, String genre) {
+
+    }
+
+}
